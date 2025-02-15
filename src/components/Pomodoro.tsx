35d7c55d3fc280c2a4f52cf3065956b7551757aa
jellysuna <nurulaisyahtajol@gmail.com>
2025-02-15 15:49:25 +0800

    alert fix

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -5,7 +5,6 @@ import PlayButton from "./PlayButton";
 import ResetButton from "./RestartButton";
 import YouTube from "react-youtube";
 import FullscreenButton from "./FullscreenButton";
-import { Link } from "react-router-dom";
 
 const Pomodoro: React.FC = () => {
   const [timerState, setTimerState] = useState({
@@ -13,8 +12,8 @@ const Pomodoro: React.FC = () => {
     isBreak: false,
     isMuted: true,
   });
-  const [time, setTime] = useState(25 * 60);
-  const [player, setPlayer] = useState<any>(null);
+  const [time, setTime] = useState(25 * 60); // Start with 25 minutes (Pomodoro)
+  const [player, setPlayer] = useState<any>(null); // Store YouTube player reference
   const [isFullscreen, setIsFullscreen] = useState(false);
 
   useEffect(() => {
@@ -26,32 +25,31 @@ const Pomodoro: React.FC = () => {
   useEffect(() => {
     if (time === 0) {
       recordSession(timerState.isBreak, timerState.isBreak ? 5 * 60 : 25 * 60);
-      handleSwitchMode();
     }
   }, [time]);
 
   const handlePlayPause = () => {
     setTimerState((prev) => ({
       ...prev,
-      isRunning: !prev.isRunning,
+      isRunning: !prev.isRunning, // Toggle timer state
     }));
   };
 
   const handleReset = () => {
     setTimerState((prev) => ({
       ...prev,
-      isRunning: false,
+      isRunning: false, // Stop timer
     }));
-    setTime(timerState.isBreak ? 5 * 60 : 25 * 60);
+    setTime(timerState.isBreak ? 5 * 60 : 25 * 60); // Reset to either 5 or 25 minutes based on mode
   };
 
   const handleSwitchMode = () => {
     setTimerState((prev) => ({
       ...prev,
-      isRunning: false,
-      isBreak: !prev.isBreak,
+      isRunning: false, // Stop the timer when switching mode
+      isBreak: !prev.isBreak, // Toggle between Pomodoro and break mode
     }));
-    setTime(timerState.isBreak ? 25 * 60 : 5 * 60);
+    setTime(timerState.isBreak ? 25 * 60 : 5 * 60); // Change the time based on mode
   };
 
   const recordSession = (isBreak: boolean, duration: number) => {
@@ -64,26 +62,29 @@ const Pomodoro: React.FC = () => {
     localStorage.setItem(key, JSON.stringify(stats));
   };
 
+  // YouTube Player onReady callback to control playback
   const onPlayerReady = (event: any) => {
     event.target.setVolume(100);
-    event.target.playVideo();
-    setPlayer(event.target);
+    event.target.playVideo(); // Start playing the video
+    setPlayer(event.target); // Save the player reference
   };
 
+  // Toggle mute state for music
   const toggleMusic = () => {
     if (player) {
       if (timerState.isMuted) {
-        player.unMute();
+        player.unMute(); // Unmute video
       } else {
-        player.mute();
+        player.mute(); // Mute video
       }
       setTimerState((prev) => ({
         ...prev,
-        isMuted: !prev.isMuted,
+        isMuted: !prev.isMuted, // Toggle mute state
       }));
     }
   };
 
+  // Fullscreen toggle function
   const toggleFullscreen = () => {
     if (!document.fullscreenElement) {
       document.documentElement
@@ -111,9 +112,10 @@ const Pomodoro: React.FC = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          position: "relative",
+          position: "relative", // Add relative positioning
         }}
       >
+        {/* "Take a Break" Button */}
         <button
           onClick={handleSwitchMode}
           style={{
@@ -131,8 +133,10 @@ const Pomodoro: React.FC = () => {
           {timerState.isBreak ? "Pomodoro" : "take a break"}
         </button>
 
+        {/* Timer */}
         <Timer isRunning={timerState.isRunning} time={time} setTime={setTime} />
 
+        {/* Buttons */}
         <div
           style={{
             display: "flex",
@@ -148,12 +152,13 @@ const Pomodoro: React.FC = () => {
           <ResetButton onClick={handleReset} />
         </div>
 
+        {/* Music Icon (Toggle Video Play/Pause) */}
         <div
           onClick={toggleMusic}
           style={{
-            position: "relative",
-            top: "-270px",
-            marginLeft: "215px",
+            position: "absolute",
+            top: "20px",
+            right: "20px",
             cursor: "pointer",
             fontSize: "36px", // Icon size
             color: "#4E4037",
@@ -188,45 +193,37 @@ const Pomodoro: React.FC = () => {
           )}
         </div>
 
-        <YouTube
-          videoId="_adXZhMCyVE"
-          opts={{
-            height: "100%",
-            width: "100%",
-            playerVars: {
-              autoplay: 1,
-              controls: 0,
-              modestbranding: 1,
-              loop: 1,
-              playlist: "_adXZhMCyVE",
-              mute: timerState.isMuted ? 1 : 0,
-            },
-          }}
-          onReady={onPlayerReady}
-          style={{ display: "none" }}
-        />
-      </div>
-      <FullscreenButton onClick={toggleFullscreen} />
-
-      {/* View Statistics Button */}
-      <Link to="/statistics">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
+        {/* YouTube Video as Background Music */}
+        <div
           style={{
-            cursor: "pointer",
-            color: "#4E4037",
             position: "absolute",
-            bottom: "20px",
-            right: "50px",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: "none", // Hide the video player
           }}
-          viewBox="0 0 16 16"
         >
-          <path d="M4 11H2v3h2zm5-4H7v7h2zm5-5v12h-2V2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1z" />
-        </svg>
-      </Link>
+          <YouTube
+            videoId="_adXZhMCyVE" // Replace with your video ID
+            opts={{
+              height: "100%",
+              width: "100%",
+              playerVars: {
+                autoplay: 1,
+                controls: 0, // Hide controls
+                modestbranding: 1,
+                loop: 1,
+                playlist: "_adXZhMCyVE", // Loop the video
+                mute: timerState.isMuted ? 1 : 0, // Mute or unmute based on state
+              },
+            }}
+            onReady={onPlayerReady}
+          />
+        </div>
+      </div>
+      {/* Fullscreen Button */}
+      <FullscreenButton onClick={toggleFullscreen} />
     </div>
   );
 };
